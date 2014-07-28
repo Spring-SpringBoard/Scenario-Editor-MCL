89be17ecebdf1ec7e6edd143bc2222683132b18e
gajop <gajo01@gmail.com>
2014-07-28 18:30:50 +0200

    initial commit

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor MCL',
+  description='Scenario Editor for MCL',
+  shortname='SE MCL',
+  version='git',
+  mutator='Official',
+  game='Scenario Editor MCL',
+  shortGame='SE MCL',
+  modtype=1,
+  depend = {  			
+  	'MechCommander: Legacy $VERSION',
+  	'Scenario Editor Core git',
+  },
+}
